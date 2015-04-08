4f1f043ccb403825c7c0ed96ddd8b6efcb2a208d
Kirk Wight <kwight@kwight.ca>
2015-04-07 20:14:17 -0700

    Enqueue the compiled `master.js` file.

--- functions.php
+++ functions.php
@@ -102,6 +102,8 @@ add_action( 'widgets_init', '_s_widgets_init' );
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
 
+	wp_enqueue_script( 'tango', get_template_directory_uri() . '/js/master.js', array(), '20150407', true );
+
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
---
+++ js/components/master.js
@@ -0,0 +1,4 @@
+/**
+ * The master JavaScript file.
+ */
+ console.log('happy to be here');
